881bb000924fa16080dc16bcc5e9cdc6e7774440
zackhitch <zmhitchcock+git@gmail.com>
2019-04-01 16:07:14 -0700

    Added new file for custom scene, tested app and it runs as expected.

---
+++ Sprites/Sprites/CustomScene.swift
@@ -0,0 +1,23 @@
+//
+//  CustomScene.swift
+//  Sprites
+//
+//  Created by Zack Hitchcock on 4/1/19.
+//  Copyright © 2019 Zack Hitchcock. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class CustomScene: SKScene {
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard !touches.isEmpty, let touch = touches.first
+            else { return }
+        
+        // Work with your touch here
+        let node = SKShapeNode(circleOfRadius: 8)
+        node.fillColor = .red
+        node.position = touch.location(in: self)
+        addChild(node)
+    }
+}
--- Sprites/Sprites/ViewController.swift
+++ Sprites/Sprites/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var skview: SKView!
+    
+    var skscene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
     }
-
-
+    
 }
 
