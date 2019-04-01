5229c1b0b751393150b1b0f49b93ca0e1cf84b73
zackhitch <zmhitchcock+git@gmail.com>
2019-04-01 16:11:27 -0700

    Stretch goal 1 completed: add random color generator.

--- Sprites/Sprites/CustomScene.swift
+++ Sprites/Sprites/CustomScene.swift
@@ -9,6 +9,16 @@
 import UIKit
 import SpriteKit
 
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(red: .random(in: 0...1),
+                       green: .random(in: 0...1),
+                       blue: .random(in: 0...1),
+                       alpha: 1.0)
+    }
+}
+
+
 class CustomScene: SKScene {
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard !touches.isEmpty, let touch = touches.first
@@ -16,7 +26,7 @@ class CustomScene: SKScene {
         
         // Work with your touch here
         let node = SKShapeNode(circleOfRadius: 8)
-        node.fillColor = .red
+        node.fillColor = .random
         node.position = touch.location(in: self)
         addChild(node)
     }
